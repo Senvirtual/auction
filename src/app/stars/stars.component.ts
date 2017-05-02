00abbea2932ef6c3afd9dfb1a1b1472a27e56291
mxf <83903944@qq.com>
2017-05-02 18:32:34 +0800

    mm

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -7,9 +7,18 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  //利用注释来声明注入操作；此处声明一个变量作容器，在product的html声明注入的对应的值
+  @Input()
+  private rating:number = 0;
+
+  private stars:boolean[];
   constructor() { }
 
   ngOnInit() {
+    this.stars=[];
+    for(let i = 1;i <= 5;i++){
+      this.stars.push(i>this.rating);
+    }
   }
 
 }
